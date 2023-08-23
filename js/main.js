c89c4c0867c963437d7c238f76bdc155741de87e
Sarthak Jha <jhasarthak9@gmail.com>
2023-08-23 20:20:44 +0530

    added back to top on scroll

--- js/main.js
+++ js/main.js
@@ -18,8 +18,29 @@ toggleButton.addEventListener("click", (event)=>{
          // toggleBtnIcon.classList = "fa fa-bars"
          toggleBtnIcon.classList.add('fa','fa-bars');
      }
-     
-         
- 
- 
  })
+
+//  BTT:
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 60px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// NAV:
